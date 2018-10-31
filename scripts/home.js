10faede41ea4f206cf4732157e3daef869b28199
Ievgenii Martynenko <ievgeniimart@gmail.com>
2018-10-31 21:14:47 +0200

    update

--- scripts/home.js
+++ scripts/home.js
@@ -248,3 +248,12 @@ function initMap() {
 // 		.addTo(controller);
 //
 // });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+        direction: 'top',
+        hoverEnabled: false
+    });
+});
